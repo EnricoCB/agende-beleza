92e67d89098a80a3125d9c903ba2bee62ce72df8
EnricoCB <enriv.budel@gmail.com>
2023-09-02 15:35:26 -0400

    Adicionando cards com js

--- js/index.js
+++ js/index.js
@@ -3,42 +3,46 @@ const saloes = [
         imagem: "/Imagens/corte-homem.jpg",
         nome: "studio sublime",
         nota: "5",
-        categoria: "cortes"
     },
     {
         imagem: "/Imagens/corte-homem.jpg",
         nome: "studio sublime",
         nota: "5",
-        categoria: "cortes"
     },
     {
         imagem: "/Imagens/corte-homem3.jpg",
         nome: "studio sublime",
         nota: "5",
-        categoria: "cortes"
     },
     {
         imagem: "/Imagens/corte-homem.jpg",
         nome: "studio sublime",
         nota: "5",
-        categoria: "cortes"
     }
 
 ]
 
 const section = document.querySelector(".saloes")
 
-saloes.forEach((text, indice) => {
+saloes.forEach((text) => {
     const div = document.createElement('div')
     div.className = 'card-saloes'
     div.innerHTML = `
     <img src="${text.imagem}" alt="imagens do salão" class="imagens-saloes">
     <div class="card-conteudo">
-        <h3>${text.nome}</h3>
-        <p>${text.nota}</p>
-        <img src="/Imagens/Star.svg" alt="estrelas" class="imagens-estrelas">
+        <div class="text-card-conteudo">
+            <h3>${text.nome}</h3>
+            <div class="text-avaliacao">
+                <p>${text.nota}</p>
+                <img src="/Imagens/Star.svg" alt="estrelas" class="imagens-estrelas">
+            </div>
+        </div>
+
+        <div class="text-card-categoria">
+        <p>Cortes</p>
+        <p>Maquiagem</p>
     </div>
-    <h5>${text.categoria}</h5>
+    </div>
     `
     section.appendChild(div)
 })
