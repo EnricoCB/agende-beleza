53214cdba1d0eee98001307389f8a1e781c6ea8a
enricocb <enriv.budel@gmail.com>
2023-09-18 00:35:09 -0400

    passando informações da página index para salões

--- js/index.js
+++ js/index.js
@@ -30,28 +30,41 @@ const saloes = [
 
 ]
 
+
+
 const section = document.querySelector(".saloes")
 
-saloes.forEach((text) => {
+saloes.forEach((text, i) => {
     const div = document.createElement('div')
     div.className = 'card-saloes'
     div.innerHTML = `
-    <img src="${text.imagem}" alt="imagens do salão" class="imagens-saloes">
-    <div class="card-conteudo">
-        <div class="text-card-conteudo">
-            <h3>${text.nome}</h3>
-            <div class="text-avaliacao">
-                <p>${text.nota}</p>
-                <img src="/Imagens/Star.svg" alt="estrelas" class="imagens-estrelas">
+    <a  href="/html/saloes.html" class="link-imagens" id="link-imagens${i}">
+        <img src="${text.imagem}" alt="imagens do salão" class="imagens-saloes">
+        <div class="card-conteudo">
+            <div class="text-card-conteudo">
+                <h3>${text.nome}</h3>
+                <div class="text-avaliacao">
+                    <p>${text.nota}</p>
+                    <img src="/Imagens/Star.svg" alt="estrelas" class="imagens-estrelas">
+                </div>
             </div>
-        </div>
 
-        <div class="text-card-categoria">
-        <p>${text.categoria1}</p>
-        <p>${text.categoria2}</p>
-    </div>
-    </div>
+            <div class="text-card-categoria">
+            <p>${text.categoria1}</p>
+            <p>${text.categoria2}</p>
+        </div>
+        </div>
+    </a>
     `
     section.appendChild(div)
 })
 
+const a = document.querySelectorAll(".link-imagens")
+a.forEach((element) =>{
+    element.addEventListener("click", () =>{
+        let num = element.id.match(/\d+/)[0]
+        const salao = saloes[Number(num)]
+        export default saloes
+    })
+
+})
---
+++ js/saloes.js
@@ -0,0 +1,2 @@
+import saloes from '/js/saloes.jsgit ';
+console.log(saloes)
