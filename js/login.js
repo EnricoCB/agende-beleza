0a075899f058608ae0ec81d50b252086c534339b
EnricoCB <enriv.budel@gmail.com>
2023-10-02 18:53:48 -0400

    adicionando script para verificação da tela login

---
+++ js/login.js
@@ -0,0 +1,74 @@
+const formulario = document.getElementById("formulario");
+const email = document.getElementById('email')
+const senha = document.getElementById('senha')
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validaEmail(email);
+    validaSenha(senha);
+
+    const formItems = formulario.querySelectorAll(".input-box")
+    console.log(formItems)
+    const formularioValido = [...formItems].every( (item) => {
+        return item.className === "input-box"
+    });
+
+    if(formularioValido){
+        alert("CADASTRADO COM SUCESSO!")
+    }
+    
+})
+
+
+email.addEventListener("blur",() => {
+    validaEmail(email);
+})
+senha.addEventListener("blur",() => {
+    validaSenha(senha);
+})
+
+
+function exibeErro(input, message){
+    const formItem = input.parentElement;
+    console.log(formItem)
+    const textMessage = formItem.querySelector("a")
+    console.log(textMessage)
+    textMessage.innerText = message;
+    formItem.className = "input-box error"
+}
+
+
+
+function validaEmail(email) {
+    const emailValue = email.value.trim()
+    emailInicio = emailValue.substring(0, emailValue.indexOf("@"));
+    dominio = emailValue.substring(emailValue.indexOf("@")+ 1, emailValue.length);
+    if (emailValue === "") {
+        exibeErro(email, 'Por favor, preencha o campo E-mail!')
+    } else if (!((emailInicio.length >=1) &&
+    (dominio.length >=3) &&
+    (emailInicio.search("@")==-1) &&
+    (dominio.search("@")==-1) &&
+    (emailInicio.search(" ")==-1) &&
+    (dominio.search(" ")==-1) &&
+    (dominio.search(".")!=-1) &&
+    (dominio.indexOf(".") >=1)&&
+    (dominio.lastIndexOf(".") < dominio.length - 1))) {
+        exibeErro(email, 'Por favor, preencha com um e-mail válido!')
+    } else {
+        const formItem = email.parentElement
+        formItem.classList = "input-box"
+    }
+}
+
+
+function validaSenha(senha) {
+    const senhaValue = senha.value.trim()
+    if (senhaValue === "") {
+        exibeErro(senha, 'Por favor, preencha o campo Senha!')
+    } else {
+        const formItem = senha.parentElement
+        formItem.classList = "input-box"
+    }
+}
+
